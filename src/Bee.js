ffc7396b0ba605d9b8b6f60c6cdfe5700dbb6d85
Michael Flis <michaelflis07@gmail.com>
2018-01-06 14:10:03 -0800

    Complete beesbeesbees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  Grub.call(this);
+  // Grub.call(this, food);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.constructor = Bee;
+
+// var newBee = new Bee();
+// console.log(newBee);
